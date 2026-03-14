2f2bece8525f36f721aa71458dcdc77ea763860c
TulioG_Projects <tuliog.projects@gmail.com>
2026-03-14 23:01:17 +0800

    task AI to create an initial starting point

---
+++ src/main/java/com/example/OperatorsRunner.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import com.example.LogicalOperatorsDemo;
+import com.example.NumberOperations;
+
+public class OperatorsRunner {
+    public static void main(String[] args) {
+        System.out.println("OperatorsRunner is importing the demos:");
+        NumberOperations.main(new String[0]);
+        LogicalOperatorsDemo.main(new String[0]);
+    }
+}
